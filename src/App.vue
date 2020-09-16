f199ef53926f774c14d65c5e48c5d9b432ce5240
mars_kn <adaadapig@gmail.com>
2020-09-16 23:49:17 +0800

    Test push and pull with Macbook Pro.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="app">
-  <div v-b-tooltip.hover.left="'I want to become super front-end programmer><'">Hello, this is Mars testing Bootstrap-vue...</div>
+  <div v-b-tooltip.hover.left="'I want to become super front-end programmer><'">This is Mars testing Bootstrap-vue</div>
 </div>
 </template>
 
