c353cb64d8d98e5deee0f605b8f28fb86b4e6a47
Mars Y Huang (EXT-TW) <mars_y_huang@trendmicro.com>
2020-09-16 18:07:11 +0800

    Initialize bootstrap-vue in project.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <div v-b-tooltip.hover.left="'I want to become super front-end programmer><'">Hello, this is Mars testing Bootstrap-vue...</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
